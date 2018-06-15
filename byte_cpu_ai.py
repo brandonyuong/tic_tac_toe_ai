3114787b2ea2cc3c77a2e7ccd842085240f34bba
Brandon Yuong <brandonyuong@gmail.com>
2018-06-14 22:51:10 -0700

    Corrected the SaveStates method 'recordState'.  Previously the algorithm was incorrect and did not update P-values for losing states.

--- byte_cpu_ai.py
+++ byte_cpu_ai.py
@@ -48,37 +48,67 @@ class SaveStates:
             return retrievedData
 
 
-    def recordState(self, numTurns):
+    def recordState(self, winnerCounter):
         # Reward winning state by assigning 1 to its P-value
+        loserCounter = winnerCounter -1
         with CursorFromConnectionFromPool() as cursor:
             cursor.execute(
                 'INSERT INTO ann_ttt (savestate, pvalue) VALUES (%s, %s)',
-                (self.statesBoard[numTurns], 1))
-        with CursorFromConnectionFromPool() as cursor:
-            cursor.execute(
-                'INSERT INTO ann_ttt (savestate, pvalue) VALUES (%s, %s)',
-                (self.statesBoard[numTurns - 1], 0))
+                (self.statesBoard[winnerCounter], 1))
         afterstate_pvalue = 1
-        numTurns = numTurns - 2
-        while numTurns >= 0:
-            alpha = 0.1 # learning rate
-            retrieved = self.retrieveState(self.statesBoard[numTurns])
+        winnerCounter = winnerCounter - 2
+        # Reward states leading to winning state with small increase to P-value
+        while winnerCounter >= 0:
+            alpha = 0.1  # learning rate
+            retrieved = self.retrieveState(self.statesBoard[winnerCounter])
             if retrieved:
                 recorded_pvalue = retrieved[2]
                 new_pvalue = recorded_pvalue + alpha * (afterstate_pvalue - recorded_pvalue)
                 with CursorFromConnectionFromPool() as cursor:
                     cursor.execute(
                         'UPDATE ann_ttt SET pvalue=%s WHERE savestate=%s',
-                        (new_pvalue, self.statesBoard[numTurns]))
+                        (new_pvalue, self.statesBoard[winnerCounter]))
                 afterstate_pvalue = new_pvalue
-                numTurns -= 1
+                winnerCounter -= 2
             else:
                 recorded_pvalue = 0.5
                 new_pvalue = recorded_pvalue + alpha * (afterstate_pvalue - recorded_pvalue)
                 with CursorFromConnectionFromPool() as cursor:
                     cursor.execute(
                         'INSERT INTO ann_ttt (savestate, pvalue) VALUES (%s, %s)',
-                        (self.statesBoard[numTurns], new_pvalue))
+                        (self.statesBoard[winnerCounter], new_pvalue))
                 afterstate_pvalue = new_pvalue
-                numTurns -= 1
+                winnerCounter -= 2
 
+        # Punish losing state by assigning 0 to its P-value
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute(
+                'INSERT INTO ann_ttt (savestate, pvalue) VALUES (%s, %s)',
+                (self.statesBoard[loserCounter], 0))
+        afterstate_pvalue = 0
+        loserCounter = loserCounter - 2
+        # Punish states leading to losing state with small decrease to P-value
+        while loserCounter >= 0:
+            alpha = 0.1 # learning rate
+            retrieved = self.retrieveState(self.statesBoard[loserCounter])
+            if retrieved:
+                recorded_pvalue = retrieved[2]
+                new_pvalue = recorded_pvalue + alpha * (afterstate_pvalue - recorded_pvalue)
+                with CursorFromConnectionFromPool() as cursor:
+                    cursor.execute(
+                        'UPDATE ann_ttt SET pvalue=%s WHERE savestate=%s',
+                        (new_pvalue, self.statesBoard[loserCounter]))
+                afterstate_pvalue = new_pvalue
+                loserCounter -= 2
+            else:
+                recorded_pvalue = 0.5
+                new_pvalue = recorded_pvalue + alpha * (afterstate_pvalue - recorded_pvalue)
+                with CursorFromConnectionFromPool() as cursor:
+                    cursor.execute(
+                        'INSERT INTO ann_ttt (savestate, pvalue) VALUES (%s, %s)',
+                        (self.statesBoard[loserCounter], new_pvalue))
+                afterstate_pvalue = new_pvalue
+                loserCounter -= 2
+
+    def getByteMove(self):
+        pass
